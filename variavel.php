8301255a17210a11842aa2d61a525ed3a7b6300f
Flavia Venturini <flaviaventurini@msn.com>
2022-11-23 15:54:45 -0300

    update

--- variavel.php
+++ variavel.php
@@ -6,6 +6,11 @@
   $gorjetas = 40;
   $diaria = $valorDia + $gorjetas;
 
-  echo "Segunda-feira: $diaria";
+  echo "Segunda-feira: $diaria\n";
+
+  $gorjetas = 15;
+  $diaria = $valorDia + $gorjetas;
+
+  echo "Terça-feira: $diaria";
 
 ?>
